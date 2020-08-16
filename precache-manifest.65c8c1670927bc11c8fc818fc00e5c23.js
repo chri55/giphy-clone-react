7c6786387c1eab539e1f5e3ff982ad0f0452d3a3
Chris Till <cpaultill@gmail.com>
2020-08-16 16:15:11 -0400

    Updates

---
+++ precache-manifest.65c8c1670927bc11c8fc818fc00e5c23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c06bc8463a1fcb42d7ed288c2742404",
+    "url": "/giphy-clone-react/index.html"
+  },
+  {
+    "revision": "8bcef03b13791cc3d4ca",
+    "url": "/giphy-clone-react/static/css/main.27ba15e3.chunk.css"
+  },
+  {
+    "revision": "be913528113a6abb6665",
+    "url": "/giphy-clone-react/static/js/2.d59d3168.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/giphy-clone-react/static/js/2.d59d3168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bcef03b13791cc3d4ca",
+    "url": "/giphy-clone-react/static/js/main.bf5362d5.chunk.js"
+  },
+  {
+    "revision": "d3c51fa8d6fc49ecbc88",
+    "url": "/giphy-clone-react/static/js/runtime-main.89140ce0.js"
+  }
+]);
